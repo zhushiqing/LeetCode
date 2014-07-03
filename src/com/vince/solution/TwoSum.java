e3db654b4788aacb8353869d72651d0b084e8906
zhushiqing <zsq66zsq@163.com>
2014-07-03 20:06:02 +0800

    the sulution of TwoSum

---
+++ src/com/vince/solution/TwoSum.java
@@ -0,0 +1,25 @@
+package com.vince.solution;
+
+public class TwoSum {
+	public int reverse(int x) {
+        int sum = 0;
+        if(x > 0){
+            while( x != 0){
+                sum = sum * 10 + x % 10;
+                x = x / 10;
+            }
+        }
+        else if( x == 0){
+            sum = x;
+        }
+        else {
+            x = -x;
+            while( x != 0){
+                sum = sum * 10 + x % 10;
+                x = x / 10;
+            }
+            sum = - sum;
+        }
+        return sum;
+        }
+}
